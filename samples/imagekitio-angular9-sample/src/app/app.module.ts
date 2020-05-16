02a40af22cc206cef1a400377730ab94a5a7b692
subhomoy <subhomoy.karmakar@gmail.com>
2020-05-16 20:44:47 +0530

    support angular 9 updated

---
+++ samples/imagekitio-angular9-sample/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { ImagekitioAngularModule } from "imagekitio-angular";
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, 
+    ImagekitioAngularModule.forRoot({
+      urlEndpoint: "<YOUR_URL_ENDPOINT>",
+      publicKey: "<YOUR_PUBLIC_KEY>",
+      authenticationEndpoint: "<YOUR_AUTH_ENDPOINT e.g http://localhost:3000/auth>"
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
