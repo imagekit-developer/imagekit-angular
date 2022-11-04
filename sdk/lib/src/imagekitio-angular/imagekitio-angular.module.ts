9cd41dba3ae331971af76a782a7ee9579f279efb
yz <yongzhen.ng@gmail.com>
2022-11-04 17:38:08 +0800

    Add ik-video component. Update imagekit-javascript version. Remove node_modules folder from sample-server.

--- sdk/lib/src/imagekitio-angular/imagekitio-angular.module.ts
+++ sdk/lib/src/imagekitio-angular/imagekitio-angular.module.ts
@@ -1,14 +1,15 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { IkImageComponent } from './ik-image/ik-image.component';
 import { IkUploadComponent } from './ik-upload/ik-upload.component';
+import { IkVideoComponent } from './ik-video/ik-video.component';
 import { ImageKitConfiguration, ImagekitService } from './imagekit.service';
 
 
 @NgModule({
-  declarations: [IkUploadComponent, IkImageComponent],
+  declarations: [IkUploadComponent, IkImageComponent, IkVideoComponent],
   imports: [
   ],
-  exports: [IkUploadComponent, IkImageComponent],
+  exports: [IkUploadComponent, IkImageComponent, IkVideoComponent],
   providers: [ ImagekitService ]
 })
 export class ImagekitioAngularModule {
