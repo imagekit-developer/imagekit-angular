f97154b2f79add965aa4dce931ef515f7e49e25f
yz <yongzhen.ng@gmail.com>
2022-11-29 22:06:22 +0800

    Change to double quotes

--- sdk/scripts/postinstall.sh
+++ sdk/scripts/postinstall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ -e ./node_modules/@types/node/index.d.ts ]]
 then
-sed -i '' '20 s/\/\/\//\/\//' ./node_modules/@types/node/index.d.ts
+sed -i "" "20 s/\/\/\//\/\//" ./node_modules/@types/node/index.d.ts
 fi
 
 if [[ -e ./node_modules/imagekit-javascript/dist/src/interfaces/index.d.ts ]]
 then
-sed -i '' '6 s/export type/export/' ./node_modules/imagekit-javascript/dist/src/interfaces/index.d.ts
+sed -i "" "6 s/export type/export/" ./node_modules/imagekit-javascript/dist/src/interfaces/index.d.ts
 fi
