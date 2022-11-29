ec331f9a96bd1ddecf116995e8f70bed6ad4a6a0
yz <yongzhen.ng@gmail.com>
2022-11-29 21:48:40 +0800

    Update postinpostinstPostinstall to check file existance

--- sdk/scripts/postinstall.sh
+++ sdk/scripts/postinstall.sh
@@ -1,2 +1,9 @@
+if [[ -e ./node_modules/@types/node/index.d.ts ]]
+then
 sed -i '' '20 s/\/\/\//\/\//' ./node_modules/@types/node/index.d.ts
+fi
+
+if [[ -e ./node_modules/imagekit-javascript/dist/src/interfaces/index.d.ts ]]
+then
 sed -i '' '6 s/export type/export/' ./node_modules/imagekit-javascript/dist/src/interfaces/index.d.ts
+fi
