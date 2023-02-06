435e3c221a4a2f6075359020e76b13bfd61d1518
yz <yongzhen.ng@gmail.com>
2023-02-06 16:12:40 +0800

    Move tests to its own folder

--- sdk/cypress.config.js
+++ sdk/cypress.config.js
@@ -4,18 +4,18 @@ module.exports = defineConfig({
   component: {
     // component options here
   },
-  fixturesFolder: './cypress/fixtures',
-  screenshotsFolder: './cypress/screenshots',
-  videosFolder: './cypress/videos',
+  fixturesFolder: './tests/cypress/fixtures',
+  screenshotsFolder: './tests/cypress/screenshots',
+  videosFolder: './tests/cypress/videos',
   env: {
     APP_HOST: 'http://localhost:4200/',
   },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      return require('./cypress/plugins/index.js')(on, config)
+      return require('./tests/cypress/plugins/index.js')(on, config)
     },
-    specPattern: 'cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: 'tests/cypress/support/e2e.js',
   },
 });
