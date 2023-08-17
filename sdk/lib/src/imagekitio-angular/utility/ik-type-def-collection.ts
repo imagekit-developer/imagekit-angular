c8d0d7a778e9877b25a6ba847e3a38dba505ce86
Nikhil Sejwal <nikhilsejwal@Nikhils-MacBook-Air.local>
2023-08-17 17:45:22 +0530

    feat: authenticator in unit test cases

--- sdk/lib/src/imagekitio-angular/utility/ik-type-def-collection.ts
+++ sdk/lib/src/imagekitio-angular/utility/ik-type-def-collection.ts
@@ -37,6 +37,7 @@ export interface IkVideoComponentOptions extends UrlOptionsBase {
 export interface IkUploadComponentOptions {
     file: File;
     fileName: string;
+    authenticator?: () => Promise<any>;
     tags?: Array<string>;
     useUniqueFileName?: boolean;
     responseFields?: Array<string>;
