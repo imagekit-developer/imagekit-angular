b02a954d470ec36c968dd3a862e051a3f5cc5b7f
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2023-12-04 11:13:56 +0530

    remove authenticationEndpoint

--- sdk/tests/sdk-tests/ik-video.component.spec.ts
+++ sdk/tests/sdk-tests/ik-video.component.spec.ts
@@ -13,7 +13,6 @@ describe("IkVideoComponent", () => {
     imageKitService = new ImagekitService({
       urlEndpoint: "https://ik.imagekit.io/company/",
       publicKey: "abc",
-      authenticationEndpoint: "https://example.com/auth"
     });
     TestBed.configureTestingModule({
       declarations: [IkVideoComponent],
@@ -44,7 +43,6 @@ describe("IkVideoComponent", () => {
     iKService = new ImagekitService({
       urlEndpoint: "https://ik.imagekit.io/company/",
       publicKey: "abc",
-      authenticationEndpoint: "http://example.com/auth"
     });
     let elRef: ElementRef;
     comp = new IkVideoComponent(elRef, iKService);
