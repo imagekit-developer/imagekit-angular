c136890900fe1a18ecc7e0cc03f72f717c4327d8
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2023-12-04 08:53:45 +0530

    updated ci version

--- sdk/src/app/app.component.ts
+++ sdk/src/app/app.component.ts
@@ -1,47 +1,39 @@
-import { Component, ViewChild } from "@angular/core";
-import { Transformation } from "imagekit-javascript/dist/src/interfaces/Transformation";
+import { Component, ViewChild } from '@angular/core';
+import { Transformation } from 'imagekit-javascript/dist/src/interfaces/Transformation';
 import { IkUploadComponent } from "../../lib/src/public_api";
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = "app";
+  title = 'app';
   path = "default-image.jpg";
   videoPath = "sample-video.mp4";
   @ViewChild('upload') uploadComponent:IkUploadComponent;
 
-  transformation: Array<Transformation> = [
-    {
-      height: "200",
-      width: "200",
-    },
-  ];
+  transformation: Array<Transformation> = [{
+     height: "200",
+     width: "200"
+  }];
 
-  flexibleTransformationOne: Array<Transformation> = [
-    {
-      height: "300",
-      width: "300",
-    },
-  ];
+  flexibleTransformationOne: Array<Transformation> = [{
+    height: "300",
+    width: "300"
+  }];
 
-  flexibleTransformationTwo: Array<Transformation> = [
-    {
-      height: "200",
-      width: "200",
-    },
-  ];
+  flexibleTransformationTwo: Array<Transformation> = [{
+    height: "200",
+    width: "200"
+ }];
 
-  videoAdvanceTransformation: Array<Transformation> = [
-    {
-      height: "200",
-      width: "600",
-      b: "5_red",
-      q: "95",
-    },
-  ];
+  videoAdvanceTransformation: Array<Transformation> = [{
+    height: "200",
+    width: "600",
+    b: "5_red",
+    q: "95"
+  }];
 
   lqipOne = { active: true, quality: 20, blur: 10 };
   lqipTwo = { active: true, quality: 20, blur: 30 };
@@ -71,66 +63,55 @@ export class AppComponent {
   };
 
   applyImgTransformationOne(res) {
-    this.flexibleTransformationOne = [
-      {
-        height: "200",
-        width: "600",
-        radius: "max",
-      },
-      {
-        height: "200",
-        width: "200",
-        rotate: "180",
-      },
-      {
-        ot: "TEST",
-        oy: "50",
-        ox: "100",
-        otc: "10C0F0",
-      },
-    ];
+    this.flexibleTransformationOne = [{
+      "height": "200",
+      "width": "600",
+      "radius": "max",
+    }, {
+      "height": "200",
+      "width": "200",
+      "rotate": "180",
+    }, {
+      "ot": "TEST",
+      "oy": "50",
+      "ox": "100",
+      "otc": "10C0F0"
+    }];
   }
 
   applyImgTransformationTwo(res) {
-    this.flexibleTransformationTwo = [
-      {
-        height: "200",
-        width: "200",
-        radius: "max",
-      },
-    ];
+    this.flexibleTransformationTwo = [{
+      "height": "200",
+      "width": "200",
+      "radius": "max",
+    }];
   }
 
   validateFileFunction(res: any) {
-    console.log("validating");
-    if (res.size < 1000000) {
-      // Less than 1mb
+    console.log('validating')
+    if(res.size < 1000000){ // Less than 1mb
       return true;
     }
     return false;
   }
 
   onUploadStartFunction(res: any) {
-    console.log("onUploadStart");
-  }
-
-  onAbortFunction(){
-    this.uploadComponent && this.uploadComponent.abort();
+    console.log('onUploadStart')
   }
 
   onUploadProgressFunction(res: any) {
-    console.log("progressing");
+    console.log('progressing')
   }
 
   handleUploadSuccess(res) {
-    console.log("File upload success with response: ", res);
+    console.log('File upload success with response: ', res);
     console.log(res.$ResponseMetadata.statusCode); // 200
     console.log(res.$ResponseMetadata.headers); // headers
     this.uploadedImageSource = res.url;
   }
 
   handleUploadError(err) {
-    console.log("There was an error in upload: ", err);
-    this.uploadErrorMessage = "File upload failed.";
+    console.log('There was an error in upload: ', err);
+    this.uploadErrorMessage = 'File upload failed.';
   }
 }
