335e314d8cae32420a1b49b88afa0c63d884090a
Swarnim Doegar <swarnim@imagekit.io>
2026-01-12 01:41:35 +0530

    Get rid of e2e test flakiness

--- test-app/playwright.config.ts
+++ test-app/playwright.config.ts
@@ -29,10 +29,6 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
