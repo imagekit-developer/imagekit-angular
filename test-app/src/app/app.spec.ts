970e9c303eb517c6a90ea8ee63c5a48d82473e6c
Swarnim Doegar <swarnim@imagekit.io>
2026-01-12 01:30:51 +0530

    Add image and video directive examples in test-app

--- test-app/src/app/app.spec.ts
+++ test-app/src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angularDx');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, test-app');
   });
 });
--- test-app/src/app/app.ts
+++ test-app/src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { Image, Video } from '@imagekit/angular';
+import { IKImageDirective, IKVideoDirective, Image, Video } from '@imagekit/angular';
 import { CommonModule } from '@angular/common';
 import { UploadTestComponent } from './upload-test';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, Image, Video, UploadTestComponent],
+  imports: [CommonModule, Image, IKImageDirective, Video, IKVideoDirective, UploadTestComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
